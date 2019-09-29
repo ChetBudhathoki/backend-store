bcbac87821954e463938de2f5ed92064baa57cc1
Chet Budhathoki <okchet@gmail.com>
2019-09-28 19:10:19 -0600

    Updated function to work on stock quantity

--- backendStoreCustomer.js
+++ backendStoreCustomer.js
@@ -1,57 +1,56 @@
-var mysql = require("mysql");;
+var mysql = require("mysql");
 var inquirer = require("inquirer");
 // var table = require("console.table");
-
-
 var connection = mysql.createConnection({
     host: "localhost",
-
     // Your port; if not 3306
     port: 3306,
-
     // Your username
     user: "root",
-
     // Your password
     password: "mySQL2019",
     database: "bamazon"
 });
-
 connection.connect(function (err) {
     if (err) throw err;
     console.log("\nWelcome to Backend Store BAmazon! Seasons Sale!\n");
    
     allProducts();
 });
-
-function allProducts() {
+var allProducts = function() {
     // query the database for all items for sale
     connection.query("SELECT * from products;", function(err, results) {
         if (err) throw err;
-        else {
+       
         // console log all products
         console.table(results);
-      }
+    
       pickProduct();
      
     }
     
 )}
-
-
-
 function pickProduct() {
+    
     inquirer
         .prompt([
         {
           name: "product",
           type: "input",
-          message: "What is the product_id you would like to buy?"
+          message: "What is the product_id you would like to buy?",
+          validate: function(value) {
+            if(isNaN(value) === false) return true;
+            else return false
+        }
         },
         {
           name: "quantity",
-          type: "input",
-          message: "How many?"
+          type: "number",
+          message: "How many?",
+          validate: function(value) {
+              if(isNaN(value) === false) return true;
+              else return false
+          }
         }
         ])
         .then(function(answer) {
@@ -59,14 +58,16 @@ function pickProduct() {
             
             var product = answer.product;
             var quantity = answer.quantity;
-            
+            // console.log(product);
+            // console.log(quantity);
             var queryProducts = "SELECT * FROM products WHERE ?";
-            var cost 
-            connection.query(queryProducts, {id: answer.product}, function(err) {
-                
+            
+            connection.query(queryProducts, {id: answer.product}, function(err, data) {
+                if (err) throw err;                
+                // console.log(data);
+                // console.log(data[0].product_name);
                 var productInfo = answer;
-                if (err) throw err;
-                if (quantity > answer.stock_quantity) {
+                if (quantity > data[0].stock_quantity) {
                     console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                     console.log("I'm sorry we don't have enough in stock, choose a smaller quantity!");
                     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
@@ -75,7 +76,7 @@ function pickProduct() {
                 
                  else {
                    
-                    if (quantity <= productInfo.quantity) {
+                    if (quantity <= data[0].quantity) {
                         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                         console.log("We have " + quantity + " " + productInfo.product + "s in stock for your order!")
                         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
@@ -90,7 +91,7 @@ function pickProduct() {
                     }
                    
             var queryUpdate = "UPDATE products SET ? WHERE ?"
-            connection.query(queryUpdate, [{stock_quantity: answer.quantity},{id: product}], function(err, res) {
+            connection.query(queryUpdate, [{stock_quantity: data[0].stock_quantity - answer.quantity},{id: product}], function(err, res) {
                  if (err) throw err;
                  else  {   
                     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");  
@@ -120,11 +121,9 @@ function pickProduct() {
                       }
                 })
                     }
-
                 
             })
          
         })
-
         
         }
